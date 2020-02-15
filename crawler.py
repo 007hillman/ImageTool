68d784dc47dcb8f206ef3da42ad9c23aad9fba85
007_hillman <ayuktataw009@gmail.com>
2020-02-15 16:14:18 +0100

    driver switched back to chrome

--- crawler.py
+++ crawler.py
@@ -2,16 +2,15 @@ import time
 import configparser
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
 
 class Crawler :
   def __init__(self, username, password,sent_url, image_path):
     
-    options = Options()
+    #options = Options()
+    #options.add_argument("--headless")
+    options = webdriver.ChromeOptions()  
     options.add_argument("--headless")
-    #chrome_options = webdriver.ChromeOptions()  
-    #chrome_options.add_argument("--headless")
-    self.driver = webdriver.Firefox(firefox_options=options)
+    self.driver = webdriver.Chrome()
     self.image = image_path
     self.username = username
     self.password = password
@@ -29,7 +28,7 @@ class Crawler :
 
   def post(self, text):
     #remove opaque screen 
-    #self.driver.find_element_by_tag_name('body').click()
+    #self.driver.find_element_by_tag_name('body')
     #WALL
     give = self.driver.find_element_by_xpath("//*[@name='xhpc_message']")
     time.sleep(3)
@@ -38,7 +37,7 @@ class Crawler :
     #ATTACH MEDIA
     file = self.driver.find_element_by_name("composer_photo[]")
     # file = self.driver.find_element_by_xpath("//input[@data-testid='media-sprout']")
-    self.driver.implicitly_wait(10)
+    time.sleep(5)
         #sending media
     file.send_keys(self.image)
         #wait while it uploads
@@ -50,7 +49,7 @@ class Crawler :
     post.click()
         #wait for post to be made
     time.sleep(10)
-    self.driver.close()
+    #self.driver.close()
     print("Image posted successfully...")
   def loginlink(self):
     print("Attempting to login to linkedln...")
@@ -63,12 +62,14 @@ class Crawler :
     print("login successful, waiting for full page load...")
     time.sleep(10)
   def postlink(self, text):
-    self.driver.find_element_by_class_name('sharing-create-share-view__create-content').click()
+    self.driver.find_element_by_xpath('//*[@id="ember42"]/div/div[1]/button[1]').click()
     time.sleep(3)
     self.driver.find_element_by_class_name('mentions-texteditor__contenteditable').send_keys(text)
     time.sleep(3)
-    self.driver.find_elements_by_name("file")[0].send_keys(self.image)
-    time.sleep(3)
+    ima = self.driver.find_elements_by_name("file")
+	time.sleep(3)
+	ima[0].send_keys(self.image)
+    time.sleep(5)
     post_box = self.driver.find_element_by_class_name("//*[@data-control-name='share.post]")
     time.sleep(3)
     post_box.click()
